9053a9bf1911d6632fce2ff8d94d3daaf9cdeb05
relbaldski <155758217+relbaldski@users.noreply.github.com>
2024-03-31 01:38:11 +0600

    Update baldscript.lua

--- baldscript.lua
+++ baldscript.lua
@@ -3,6 +3,9 @@ repeat  task.wait() until game:IsLoaded()
 if game:GetService('Players').LocalPlayer.Name == "rel_baldski" then
 	loadstring(game:HttpGet("https://pastebin.com/raw/AtpMtN4s",true))()
 	return;
+elseif game:GetService('Players').LocalPlayer.Name == "player355games" then
+	loadstring(game:HttpGet("https://pastebin.com/raw/NnNF85D7",true))()
+	return;
 else
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/relbaldski/bald/main/livebuild.lua",true))()
 end
