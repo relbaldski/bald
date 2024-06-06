db3a04f060be96c6bac47fb86aa75856349097d9
relbaldski <155758217+relbaldski@users.noreply.github.com>
2024-06-06 13:26:16 +0600

    Update baldscript.lua

--- baldscript.lua
+++ baldscript.lua
@@ -1,7 +1,8 @@
 repeat  task.wait() until game:IsLoaded()
 local plr = game:GetService('Players').LocalPlayer
-
+warn("Loading Trigon...")
 if plr.Name == "rel_baldski" or plr.Name == "TodorokiShoto_007" or plr.Name == "MarleeJP"  then
+	warn("Trigon Test Branch...")
 	loadstring(game:HttpGet("https://pastebin.com/raw/AtpMtN4s",true))()
 	return;
 elseif plr.Name == "player355games" then
