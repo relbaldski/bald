0416b8701dc30dbf95e171d558a238a24a9818f2
relbaldski <155758217+relbaldski@users.noreply.github.com>
2024-08-21 16:17:34 +0600

    Update baldscript.lua

--- baldscript.lua
+++ baldscript.lua
@@ -4,8 +4,8 @@ warn("Loading Trigon...")
 if plr.Name == "rel_baldski"  then
 	warn("Trigon Test Branch...")
 	--loadstring(game:HttpGet("https://raw.githubusercontent.com/relbaldski/bald/main/livebuild.lua",true))()
-	loadstring(game:HttpGet("https://pastebin.com/raw/yeJWgwLw",true))()
-	--loadstring(game:HttpGet("https://pub.microbin.eu/raw/spider-emu-turtle",true))()
+	--loadstring(game:HttpGet("https://pastebin.com/raw/yeJWgwLw",true))()
+	loadstring(game:HttpGet("https://trigonevo.fun/x/test",true))()
 	--loadstring(game:HttpGet("https://pastebin.com/raw/AtpMtN4s",true))()
 	return;
 elseif plr.Name == "player355games" then
