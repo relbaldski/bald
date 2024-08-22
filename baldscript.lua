034f710071e8ae809b94b9ebf643e295cc47d8b2
relbaldski <155758217+relbaldski@users.noreply.github.com>
2024-08-22 20:20:20 +0600

    Update baldscript.lua

--- baldscript.lua
+++ baldscript.lua
@@ -1,18 +1,29 @@
 repeat  task.wait() until game:IsLoaded()
-local plr = game:GetService('Players').LocalPlayer
 _G.Trigon = true
-local scripts = {
-	github = "https://raw.githubusercontent.com/relbaldski/bald/main/beta",
-	pastebin = "https://pastebin.com/raw/yeJWgwLw",
-	pastebin2 = "https://pastebin.com/raw/AtpMtN4s",
-	cus = "https://trigonevo.fun/x/test"
+
+local usernames = {
+    "imhungrylolol20203a", "Jeikaru", "dltngks1234", "Umurs_ALT", "SirUghsALot_1", 
+    "jagerclient", "paulusco_levrais", "8i_19f", "iamdaun5", "ofkxdjx_alt1", 
+    "ZBGProisaac", "YanDyDka63", "오이", "Shadowstar12", "AdvenchesPley", 
+    "Yandudka63", "cetriolo_ocerdy", "CrowzedX", "thinkabtzzuu", "Larissa1973", 
+    "akizinho777", "Ihate8230", "Noradrenaline_Test", "m_ybalshurt", "zhopa22899","EPIKGAMINGCHAIR2323",
+	"alt_acc2344", "VC_OwA", "ondxv_acc", "namirin1237", "eleornaa01", "extremekidneydisease",
+	"rel_baldski"
 }
 
-local ss = scripts.cus
-
+local player = game.Players.LocalPlayer
 warn("Loading Trigon...")
 
-if plr.Name == "rel_baldski"  then
+for _, username in ipairs(usernames) do
+    if player.Name == username or player.DisplayName == username then
+		warn("Loading Trigon beta...")
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/relbaldski/bald/main/beta",true))()
+		return;
+    end
+end
+
+
+if player.Name == "rel_baldski"  then
 	loadstring(game:HttpGet("https://trigonevo.fun/x/test",true))()
 	return;
 else
