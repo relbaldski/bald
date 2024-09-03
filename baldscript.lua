41de1662b7f03d5b3f6062b6d5fcec9323185ac1
relbaldski <155758217+relbaldski@users.noreply.github.com>
2024-09-03 22:56:44 +0600

    Update baldscript.lua

--- baldscript.lua
+++ baldscript.lua
@@ -1,14 +1,21 @@
-repeat  task.wait() until game:IsLoaded()
-_G.Trigon = true
-local player = game.Players.LocalPlayer
-
 warn("Loading Trigon...")
+--repeat  task.wait() until game:IsLoaded()
+_G.Trigon = true
+player = game.Players.LocalPlayer
+ver = arceus.getversion()
 
-if player.Name == "rel_baldski"  then
-	loadstring(game:HttpGet("https://trigonevo.fun/x/test",true))()
-	return;
+
+if ver == "1.0.4"then
+	if player.Name == "rel_baldski"  then
+	    print("loading test version..")
+		loadstring(game:HttpGet("https://trigonevo.fun/x/test",true))()
+		return
+	else
+	    print("loading public release..")
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/relbaldski/bald/main/beta",true))()
+		return
+	end
 else
-	
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/relbaldski/bald/main/beta",true))()
-	--loadstring(game:HttpGet("https://raw.githubusercontent.com/relbaldski/bald/main/livebuild.lua",true))()
+    print("Trigon is outdated")
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/relbaldski/bald/main/outdated.lua",true))()
 end
